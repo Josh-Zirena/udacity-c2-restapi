9ea7f1ce546667c8744c5cd60cde3e368e191072
Josh Zirena <JoshZirena@gmail.com>
2020-05-29 19:19:16 +0200

    feat: add env variables to config.ts

--- src/aws.ts
+++ src/aws.ts
@@ -3,6 +3,7 @@ import { config } from './config/config';
 
 const c = config.dev;
 
+
 //Configure AWS
 var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
 AWS.config.credentials = credentials;
